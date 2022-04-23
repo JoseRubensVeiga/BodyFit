7f69537ad6ec73d89e317c371679af6127929a86
José Veiga <jose.rubens.veiga@gmail.com>
2022-04-23 00:25:35 -0300

    criando tela de login

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignInComponent } from './components/sign-in';
 
 @NgModule({
   declarations: [SignInComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
 })
 export class AuthModule {}
